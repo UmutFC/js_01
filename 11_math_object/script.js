ec09387934418749ffeb44926cfdbb91665b9718
Furkan Umut <135552905+UmutFC@users.noreply.github.com>
2023-07-07 23:48:50 +0300

    commit
    commit

---
+++ 11_math_object/script.js
@@ -0,0 +1,33 @@
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.5);
+
+// Round Up
+x = Math.ceil(4.2);
+
+// Round Down
+x = Math.floor(4.8);
+
+// .pow(x, y); x power of y
+x = Math.pow(2, 3);
+
+// Find minimum or maximum
+x = Math.min(2, 5, 3);
+x = Math.max(2, 5, 3);
+
+// Random number between 0 and 1
+x = Math.random();
+
+// Decimal between 0 to 10
+//x = x * 10;
+
+// 1 to 11
+//x = x * 10 + 1;
+
+x = Math.floor(x * 100 + 1);
+
+console.log(x);
